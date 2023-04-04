93e2fc1312ff18d39ae417eec715679d35252328
chaitanya <ck379291@gmail;.com>
2023-04-04 07:19:07 -0700

    practice

--- app/src/main/java/com/example/ragistration/Login_Activity.kt
+++ app/src/main/java/com/example/ragistration/Login_Activity.kt
@@ -3,21 +3,41 @@ package com.example.ragistration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.ragistration.Factory.SinUpFactory
+import com.example.ragistration.ViewModel.SinUpViewModel
 import com.example.ragistration.databinding.ActivityLoginBinding
+import com.example.ragistration.repository.SinUpRepository
 
 class Login_Activity : AppCompatActivity() {
     private lateinit var binding:ActivityLoginBinding
+    lateinit var factory:SinUpFactory
+    lateinit var view:SinUpViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        factory=SinUpFactory(SinUpRepository,this)
+        view= ViewModelProvider(this,factory)[SinUpViewModel::class.java]
+
 
         binding.btnSinupLogin.setOnClickListener {
             val intent = Intent(this, SinUp_Activity::class.java)
             startActivity(intent)
         }
 
+        binding.btnLogin.setOnClickListener {
+            var mobile= binding.editMobnoLogin.text.toString()
+
+            if(view.getdata()!=mobile){
+                Toast.makeText(this, "Please enter correct mobile number", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                startActivity(Intent(this,MainActivity::class.java))
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/ragistration/ViewModel/SinUpViewModel.kt
+++ app/src/main/java/com/example/ragistration/ViewModel/SinUpViewModel.kt
@@ -7,5 +7,10 @@ import com.example.ragistration.repository.SinUpRepository
 class SinUpViewModel(private val sharePrefRepository: SinUpRepository, private val context:Context):ViewModel() {
     fun savedata(mobno:Int){
         sharePrefRepository.getprefrence(context).edit().putInt("Mobile",mobno).commit()
+
+    }
+    fun getdata():String{
+        var monumber= sharePrefRepository.getprefrence(context).getString("Mobile","")
+        return monumber.toString()
     }
 }
